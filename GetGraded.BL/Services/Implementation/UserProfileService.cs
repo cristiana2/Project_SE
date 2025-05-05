0d624a67568f4a8b464a7f99fbe749eb65622445
cristiana2 <epuran_cristiana@yahoo.com>
2025-05-05 23:04:50 +0300

    edit BL

---
+++ GetGraded.BL/Services/Implementation/UserProfileService.cs
@@ -0,0 +1,52 @@
+﻿using GetGraded.BL.Services.Interface;
+using GetGraded.BL.UserProfileStrategy;
+using GetGraded.DAL.Repository.Interface;
+using GetGraded.Models.Models;
+using GetGraded.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace GetGraded.BL.Services.Implementation
+{
+    public class UserProfileService : IUserProfileService
+    {
+        private readonly IUserProfileRepository _userProfileRepository;
+        private readonly IUserProfileStrategy _userProfileStrategy;
+        public UserProfileService(IUserProfileRepository userProfileRepository, IUserProfileStrategy userProfileStrategy)
+        {
+            _userProfileRepository = userProfileRepository;
+            _userProfileStrategy = userProfileStrategy;
+        }
+
+
+        public async Task CreateAccount(UserProfileView userprofile)
+        {
+
+            await _userProfileRepository.SaveUserLoginDetails(new UserLoginDetails()
+            {
+                Email = userprofile.Email,
+                Password = userprofile.Password
+            });
+            var id = await _userProfileRepository.FindUserProfile(userprofile.Email);
+
+            await _userProfileRepository.SaveUserProfile(new UserProfile()
+            {
+                FirstName = userprofile.FirstName,
+                MiddleName = userprofile.MiddleName,
+                LastName = userprofile.LastName,
+                BirthDate = userprofile.BirthDate,
+                UserLoginId = id.Value,
+                RoleId = userprofile.RoleId,
+                DepartmentId = userprofile.DepartmentId,
+            });
+            await _userProfileStrategy.SaveAdditionalProperties(id.Value, userprofile);
+        }
+        public async Task<bool> CheckEmailAvailability(string email)
+
+        {
+            var userProfile = await  _userProfileRepository.FindUserProfile(email);
+            return userProfile == null ? false : true ;
+        }
+
+    }
+        
+}

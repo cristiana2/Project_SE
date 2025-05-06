edccda1692cafb8a01a59922bf2114555fa09c5d
Paduretu Andrei-Stefan <spaduretu@yahoo.com>
2025-05-06 19:40:47 +0300

    Add TODO comment and logging using directive in Program.cs

--- Project_SE/Program.cs
+++ Project_SE/Program.cs
@@ -1,5 +1,7 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Extensions.Logging;
 
+// TODO: Implement additional API endpoints here.
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
